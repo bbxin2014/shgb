e4779389ac907df62e0e60b5b171143cd6c39a7e
bbxin2014 <bbxin2014@163.com>
2014-06-15 18:40:49 +0800

    20140615

---
+++ live.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+document.writeln("<script type=\"text/javascript\">");
+document.writeln("var _bdhmProtocol = ((\"https:\" == document.location.protocol) ? \" https://\" : \" http://\");");
+document.writeln("document.write(unescape(\"%3Cscript src=\'\" + _bdhmProtocol + \"hm.baidu.com/h.js%3Fddb9c4142205735e4d45485b22797132\' type=\'text/javascript\'%3E%3C/script%3E\"));");
+document.writeln("</script>");
